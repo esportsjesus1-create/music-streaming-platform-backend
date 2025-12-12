855b09be9c248a1f16a91274b9a410b10b4ce104
esportsjesus1-create <esportsjesus1@gmail.com>
2025-12-11 17:44:56 -0800

    Add database connection function using mongoose

---
+++ src/config/database.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const connectDatabase = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.DATABASE_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log(`📛 MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`❌ Database Connection Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDatabase;
